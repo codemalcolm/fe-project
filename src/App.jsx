fc1ca347e05b6b76ffc88b39a19947d5e336308a
VladimirMasek <vladam3636@gamil.com>
2024-10-25 20:33:14 +0200

    zkouška git push

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { Box } from "@chakra-ui/react"
-import { Link } from "react-router-dom"
-
+import { Box } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 function App() {
-
-  return (
-    <Box>
-    My box
-
-    </Box>
-  )
+  return <Box>My box Vladimír</Box>;
 }
 
-export default App
+export default App;
