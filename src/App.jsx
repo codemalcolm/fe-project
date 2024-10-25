c8333a0cfb62c62286cf7f438d09cf06c57df7f8
VladimirMasek <vladam3636@gamil.com>
2024-10-25 20:50:08 +0200

    zkouška druhý account

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 function App() {
-  return <Box>My box Vladimír</Box>;
+  return <Box>My box VM</Box>;
 }
 
 export default App;
